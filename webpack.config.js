4025bc775a00b1c208d8c781adc184dd3039a8c1
Frank Mitchell <me@frankmitchell.org>
2020-08-15 10:07:10 -0400

    Wire up basic SCSS processing and provide a HTML template.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunksHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlInlineCssWebpackPlugin = require('html-inline-css-webpack-plugin').default;
 
 module.exports = (_, argv) => {
   const isProduction = argv.mode === 'production';
@@ -7,11 +9,23 @@ module.exports = (_, argv) => {
   const config = {
     entry: './src',
     devtool: isProduction ? undefined : 'source-map',
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }],
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: 'src/index.html',
       }),
       new InlineChunksHtmlPlugin(HtmlWebpackPlugin, ['main']),
+      new MiniCssExtractPlugin(),
+      new HtmlInlineCssWebpackPlugin(),
     ],
     devServer: {
       host: '127.0.0.1',
