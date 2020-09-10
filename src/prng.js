836c9cbce2039927a0920dd5e8b1448a730f6a68
Frank Mitchell <me@frankmitchell.org>
2020-09-10 11:58:41 -0400

    Remove my custom PRNG and save 110 bytes.

--- src/prng.js
+++ src/prng.js
@@ -1,26 +1,6 @@
-const Utils = require('./utils');
-
 const PRNG = {};
 
-let s1 = Utils.timestamp();
-let s2 = Utils.timestamp();
-let s3 = Utils.timestamp();
-
-const clamp = (value) => Math.min(Math.max(1, value), 30000);
-
-PRNG.random = () => {
-  s1 = (171 * s1) % 30269;
-  s2 = (172 * s2) % 30307;
-  s3 = (170 * s3) % 30323;
-
-  return ((s1 / 30269) + (s2 / 30307) + (s3 / 30323)) % 1;
-};
-
-PRNG.seed = (v1, v2, v3) => {
-  s1 = clamp(v1 ? v1 : Utils.timestamp());
-  s2 = clamp(v2 ? v2 : Utils.timestamp());
-  s3 = clamp(v3 ? v3 : Utils.timestamp());
-};
+PRNG.random = Math.random;
 
 PRNG.shuffle = (array) => {
   const result = array.slice();
